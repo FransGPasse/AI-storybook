368424c96195034f20f0f083621a3a01d4993465
Frans Gustavson Påsse <frans.gop@hotmail.com>
2022-12-23 18:25:11 +0100

    First commit

---
+++ src/components/HomePage.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="homepage">
+    <h1>Welcome to the AI storybook</h1>
+  </div>
+</template>
+
+<style scoped>
+.homepage {
+  display: flex;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+h1 {
+  background-color: #f3ec78;
+  background-image: linear-gradient(90deg, #f3ec78, #fd4343);
+  background-size: 100%;
+  -webkit-background-clip: text;
+  -moz-background-clip: text;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+  -moz-text-fill-color: transparent;
+
+  font-size: 5rem;
+  text-align: center;
+}
+</style>
